fadca337dd416f35e3fc6a2943f5dbfc3637fb0f
BrayanStivenLondono <166665056+BrayanStivenLondono@users.noreply.github.com>
2025-06-03 22:39:13 +0200

    mantenimiento

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->global(MaintenanceMode::class);
+        $middleware->append(MaintenanceMode::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
