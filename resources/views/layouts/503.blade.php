b92a076df98a836eb9f8d557b4a74e0fba005ffd
BrayanStivenLondono <166665056+BrayanStivenLondono@users.noreply.github.com>
2025-06-03 22:57:03 +0200

    mantenimiento

---
+++ resources/views/layouts/503.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Sitio en mantenimiento</title>
+</head>
+<body style="text-align: center; font-family: sans-serif; padding-top: 100px;">
+<h1>🛠 Sitio en mantenimiento</h1>
+<p>Estamos trabajando para mejorar tu experiencia. Vuelve pronto.</p>
+</body>
+</html>
